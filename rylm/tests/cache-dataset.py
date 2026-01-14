22407ac95a88a724bdd3bedd792a169582b5885b
chrisiacovella <chris.iacovella@gmail.com>
2026-01-13 18:45:43 -0800

    hmm dataset cache is ended up in site packages

--- rylm/tests/cache-dataset.py
+++ rylm/tests/cache-dataset.py
@@ -3,13 +3,10 @@
 
 import pathlib
 # first see if the data.txt exists
-import rylm.tests as tests
 
 # get the path to the tests directory
-from rylm.utils import get_path_string
-root_path = get_path_string(tests)
 
-dir_path = pathlib.Path(f"{root_path}/dataset_cache")
+dir_path = pathlib.Path(f"rylm/tests/dataset_cache")
 data_file_path = pathlib.Path(dir_path / "data.txt")
 if not data_file_path.exists():
     dir_path.mkdir(parents=True, exist_ok=True)
