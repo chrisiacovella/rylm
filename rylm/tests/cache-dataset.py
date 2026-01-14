52c370444d87fff62655818712ee39dff38769c0
chrisiacovella <chris.iacovella@gmail.com>
2026-01-13 18:12:46 -0800

    testing out setting up caching in the CI

---
+++ rylm/tests/cache-dataset.py
@@ -0,0 +1,16 @@
+# this is just a test file that will write out some data to the cache
+# to see if caching is working
+
+import pathlib
+# first see if the data.txt exists
+dir_path = pathlib.Path("./cache_test_dir")
+data_file_path = pathlib.Path(dir_path / "data.txt")
+if not data_file_path:
+    dir_path.mkdir(parents=True, exist_ok=True)
+    with open(data_file_path, "w") as f:
+        f.write("This is a test file for caching.\n")
+    print(f"Created cache file at {data_file_path}")
+else:
+    with open(data_file_path, "r") as f:
+        content = f.read()
+    print(f"Cache file exists. Content:\n{content}")
